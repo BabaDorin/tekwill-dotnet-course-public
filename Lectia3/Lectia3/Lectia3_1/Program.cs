b67490b53e9ffa9e76b0988ba100e4cce7f95f38
dorin_baba <dorin.baba@endava.com>
2023-06-23 20:36:06 +0300

    Lectia 3

---
+++ Lectia3/Lectia3/Lectia3_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lectia3_1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            // 1. Preluam de la tastatura si convertim in numar prima valoare
+            // introdus de catre utilizator in consola.
+
+            // Variabila => un spatiu in memoria ram
+            Console.Write("Introdu, te rog frumos, primul numar: ");
+            double PrimulNumar = double.Parse(Console.ReadLine());
+
+            // 2. Preluam de la tastatura si convertim in numar a doua valpoare
+            Console.Write("Introdu, te rog frumos, al doilea numar: ");
+            double alDoileaNumar = double.Parse(Console.ReadLine());
+
+            // 3. Calculam suma numerelor
+            double suma = PrimulNumar + alDoileaNumar;
+
+            // 4. Afisam rezultatul obtinut la ecran
+            Console.WriteLine($"Suma numerelor este: {suma}");
+
+            Console.ReadLine();
+        }
+    }
+}
